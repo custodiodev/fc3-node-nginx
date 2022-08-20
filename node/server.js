717d8f010b8f4e29b47cd139973c3e9784baeeb2
bacustodio <bacustodio@hotmail.com>
2022-08-20 06:35:02 -0300

    Desafio Docker com Ngixn - MySql - NodeJs

---
+++ node/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = process.env.APP_PORT || 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+
+app.get('/', (req, res) => {
+  const name = `Bruno ` + Math.random()
+
+  connection.query(`INSERT INTO people (nome) VALUES ('${name}')`)
+
+  connection.query(`SELECT * FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Up on:', port);
+})
